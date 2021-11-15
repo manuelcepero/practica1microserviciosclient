dba14b03a25d2c398c6a3782987951b78e6409b8
Manuel Cepero Sánchez <manuel.cepero@hotmail.com>
2021-11-15 10:54:05 +0100

    Subida del proyecto

---
+++ src/main/java/com/nttbootcamp/practica1microservicioscliente/Practica1microserviciosclienteApplication.java
@@ -0,0 +1,15 @@
+package com.nttbootcamp.practica1microservicioscliente;
+
+import org.springframework.boot.SpringApplication;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+public class Practica1microserviciosclienteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Practica1microserviciosclienteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nttbootcamp/practica1microservicioscliente/Practica1microserviciosclienteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nttbootcamp.practica1microservicioscliente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Practica1microserviciosclienteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
